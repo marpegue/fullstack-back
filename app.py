cfd978bbba53441800a70d9a3d66e87f8418db03
Marta <marta.pelufo2903@gmail.com>
2023-01-05 18:09:35 +0100

    Creado nuevo método de la API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """ Archivo de configuración de la aplicación """
 from flask import Flask
-from flask import request, redirect
+from flask import request, redirect, Response
 import persistencia
 
 app = Flask(__name__)
@@ -13,3 +13,14 @@ def pizza():
     print(nombre + " " + apellidos)
     persistencia.guardar_pedido(nombre, apellidos)
     return redirect("http://localhost/M1-U1%20Marta%20Pelufo/solicita_pedido.html", code=302)
+
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    size = request.form.get("size")
+    mensaje = "Disponible"
+    if size == "S":
+        mensaje = "No disponible"
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
