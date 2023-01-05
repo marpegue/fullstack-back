abb899921114e6253e81fa79a715302db1de2ba2
Marta <marta.pelufo2903@gmail.com>
2023-01-05 17:27:00 +0100

    Se corrige persistencia.py para que satisfaga el test

--- persistencia.py
+++ persistencia.py
@@ -6,5 +6,5 @@ def guardar_pedido(nombre, apellidos):
     Método guardar pedido
     """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-" +nombre + " " + apellidos + "\n")
         file.close()
